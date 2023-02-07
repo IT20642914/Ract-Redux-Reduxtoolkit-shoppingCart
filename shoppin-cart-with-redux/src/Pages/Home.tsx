2040015b4cbb46e76973ceb5241680e23a002ec2
avishaka05 <it20642914@my.sliit.lk>
2023-02-07 11:01:52 +0530

    completed with slice

--- shoppin-cart-with-redux/src/Pages/Home.tsx
+++ shoppin-cart-with-redux/src/Pages/Home.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../Redux/Reducers/counterSlice';
+import { RootState } from '../Redux/Reducers/RootReducer';
+import { AppDispatch } from '../Redux/Store';
 const Home = () => {
+  const counter = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <div>
-      <h1>Home</h1>
+      <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment(1))}>Increment</button>
+      <button onClick={() => dispatch(decrement(1))}>Decrement</button>
+    </div>
     </div>
   )
 }
---
+++ shoppin-cart-with-redux/src/Redux/Reducers/RootReducer.tsx
@@ -0,0 +1,8 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import counterSlice from './counterSlice';
+
+const rootReducer = combineReducers({
+  counter: counterSlice,
+});
+export type RootState = ReturnType<typeof rootReducer>;
+export default rootReducer;
