4a790cfb065b524662f28e5245c64e179267689a
avishaka05 <it20642914@my.sliit.lk>
2023-02-07 12:44:12 +0530

    modify the code

---
+++ shoppin-cart-with-redux/src/Redux/Reducers/productSlice.tsx
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+
+interface Product {
+    id: number,
+    name: string,
+    price: number
+  }
+  interface PorductState{
+    products:Product[];
+    }
+    
+const initialState :PorductState= {
+    products: [],
+  };
+  const productSlice = createSlice({
+    name: 'products',
+    initialState,
+    reducers: {
+      getProductList: (state, action: PayloadAction<Product[]>) => {
+        state.products=action.payload;
+       
+      }
+     
+    },
+  });
+  
+  export const { getProductList } = productSlice.actions;
+  export default productSlice.reducer;
